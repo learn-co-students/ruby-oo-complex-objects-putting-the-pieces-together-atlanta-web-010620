6e0cb0fa776bd068781147ed4bd2ad616adf4315
tylergreason <tyler.greason@protonmail.com>
2020-01-08 08:49:17 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -33,9 +33,22 @@ class Book
     def turn_page 
         puts "Flipping the page...wow, you read fast!"
     end
-    
-
-
-
 end
+
+
+# the above can be simplified as such : 
+
+class AnotherBook 
+    attr_accessor :author, :page_count, :genre 
+    attr_reader :title 
+
+    def initialize(title) 
+        @title = title 
+    end 
+    def turn_page 
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+    
 
